3c627e7a2aab7a8377ef01f49f2ad4e33871f3d6
John Neil Soo <johnneilsoo@gmail.com>
2016-06-30 10:18:05 +0800

    UI Changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,5 +13,13 @@ $(document).on("ready page:change", function() {
     step: 1
   });
   
-  
+  var jumboHeight = $('.jumbotron').outerHeight();
+  function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+  }
+
+  $(window).scroll(function(e){
+      parallax();
+  });
 });
