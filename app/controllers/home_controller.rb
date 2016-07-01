eb7752d29e65c0e5cf451390dbfc33145d0c6333
John Neil Soo <johnneilsoo@gmail.com>
2016-07-01 16:13:47 +0800

    UI Changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
   
   def index
     @restaurants = Restaurant.includes(:ratings).order('ratings.rate desc').where(status: 'Accepted').limit(8)
-    @cuisines = Cuisine.all.limit(8)
+    @cuisines = Cuisine.joins(:foods).group("foods.cuisine_id").order("count(foods.cuisine_id) desc").limit(4)
+#    @cuisines = Cuisine.all.limit(8)
     @ratings = Rating.order(created_at: :desc).limit(4)
   end
   
