5b6c245d18e379ce86ed40e95182466f5ba2833f
John Neil Soo <johnneilsoo@gmail.com>
2016-07-05 10:35:47 +0800

    Added login with facebook

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704044207) do
+ActiveRecord::Schema.define(version: 20160705020246) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -140,6 +140,8 @@ ActiveRecord::Schema.define(version: 20160704044207) do
     t.string   "profile_picture_url"
     t.text     "location"
     t.string   "avatar"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
