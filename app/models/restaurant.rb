d6a91507a77190bf126aa837f57aacb8f9186bd8
Alfie Mendoza <alfie.mendoza@adish.co.jp>
2016-07-07 15:15:38 +0800

    Schedule Display Update

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -20,7 +20,25 @@ class Restaurant < ActiveRecord::Base
   end
   
   def is_open?
+    scheds = schedules.group_by{|s| [s.day]}
+    @hours = ""
+
+    mappings = {["Sunday"] => 0, ["Monday"] => 1, ["Tuesday"] => 2, ["Wednesday"] => 3, ["Thursday"] => 4, ["Friday"] => 5, ["Saturday"] => 6}
+    scheds = scheds.map {|k, v| [mappings[k], v] }.to_h
+    scheds = scheds.sort_by{ |k,v| k }.to_h
     
+    today = DateTime.now
+
+    scheds.each do |key, value|
+      if !scheds.keys.include? today.wday
+        return "Closed"
+      else
+        value.each do |val|
+          val.opening
+        end
+      end
+    end
+    return "Closed"
   end
   
   def sched
@@ -29,12 +47,16 @@ class Restaurant < ActiveRecord::Base
 
     mappings = {["Sunday"] => 0, ["Monday"] => 1, ["Tuesday"] => 2, ["Wednesday"] => 3, ["Thursday"] => 4, ["Friday"] => 5, ["Saturday"] => 6}
     scheds = scheds.map {|k, v| [mappings[k], v] }.to_h
-    scheds = scheds.sort_by{ |k,v| k }
+    scheds = scheds.sort_by{ |k,v| k }.to_h
 
     scheds.each do |key, value|
       @hours << "#{Date::DAYNAMES[key]} "
-      value.each do |val| 
-        @hours << "#{val.opening} - #{val.closing}"
+      value.each_with_index do |val, index|
+        if value.count > 1 && index < value.count-1
+          @hours << "#{val.opening} - #{val.closing}, "
+        else
+          @hours << "#{val.opening} - #{val.closing} "
+        end
       end
     end
 
