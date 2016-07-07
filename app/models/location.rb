6cd33dd0c542dd2f82606dbd7e84834ca40257d1
John Neil Soo <johnneilsoo@gmail.com>
2016-07-07 12:39:52 +0800

    UI Changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,8 @@
 class Location < ActiveRecord::Base
   belongs_to :restaurant
-  reverse_geocoded_by :latitude, :longitude, :address => :address, unless: :have_address?
-  geocoded_by :address, if: :have_address?
-  after_validation :reverse_geocode, unless: :have_address?
+  reverse_geocoded_by :latitude, :longitude, :address => :address
+  geocoded_by :address
+  after_validation :reverse_geocode
   after_validation :geocode, if: :have_address?
   
   def have_address?
