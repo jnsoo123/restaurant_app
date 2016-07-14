743ca43785bb9c8687cb498b0b8a8c7c537fe4f7
Alfie Mendoza <alfie.mendoza@adish.co.jp>
2016-07-14 16:55:02 +0800

    Partial Owner Feature

--- spec/features/owner_feature_spec.rb
+++ spec/features/owner_feature_spec.rb
@@ -13,9 +13,60 @@ feature "User Interface" do
     within(:css, '.navbar-right') do
       click_link "Login"
     end
-    fill_in 'Username', :with => user1.username
-    fill_in 'password', :with => user1.password
+    fill_in 'Username', :with => owner1.username
+    fill_in 'password', :with => owner1.password
     click_button('Login')
   end
+  
+  
+  context "Owner Interface" do
+    before(:each) do
+      click_link "#{owner1.name}"
+      click_link "My Restaurants"
+    end
+    
+    context "Restaurant Page" do
+      before(:each) do
+        find(:xpath, '//*[@id="dashboard-owner"]/div/div[2]/div/div[2]/div[1]/a').click
+      end
+      
+      scenario "Display Restaurant Edit Page" do
+        expect(page).to have_content("Edit Information")
+        expect(page).to have_content("Edit Map Location")
+        expect(page.current_path == owner_resto_edit_path(restaurant1.id)).to be true
+      end
+      
+      scenario "Edit Restaurant" do
+        fill_in "restaurant_contact", with: "I changed this"
+        click_button "Update Restaurant"
+        expect(find("#restaurant_contact").value).to eq("I changed this")
+      end
+    end
+    
+    context "Add Restaurant Page" do
+      before(:each) do
+        click_link "Add Restaurants"
+      end
+      
+      scenario "Displays Add Restaurant Page" do
+        expect(page).to have_content("Create New Restaurant")
+      end
+      
+      scenario "Add Restaurant" do
+        
+      end
+    end
+        
+    scenario "Messages Page" do
+      click_link "Messages"
+      expect(page).to have_content("My Messages")
+    end
+    
+    scenario "Return to Home Page" do
+      click_link "Go Back to Mainpage"
+      expect(page.current_path == home_path).to be true
+    end
+  end
+  
   
 end
