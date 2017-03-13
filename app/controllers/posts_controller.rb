e48b3f27bbc89d1de4ed19456249b15571373380
John Neil Soo <neil.soo@adish.co.jp>
2017-03-14 07:25:34 +0800

    something changed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,63 @@
-class PostsController < ApplicationController 
+class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: :show_more
   respond_to :js
-  
-  def new 
+
+  def new
     @post = Post.new
     @post.restaurant = current_user.restaurants.find(params[:resto_id])
     respond_with(@post)
   end
-  
-  def create 
+
+  def create
     @post = Post.new(post_params)
     @post.restaurant = current_user.restaurants.find(params[:resto_id])
     if @post.save
       @posts = @post.restaurant.posts.page params[:page]
       respond_with(@posts)
     else
-      @err = "<dl><dt>#{t('.failurestart')}</dt>" 
+      @err = "<dl><dt>#{t('.failurestart')}</dt>"
       @post.errors.full_messages.map { |msg| @err << "<dd>#{msg}</dd>" }
       @err << "</dl>"
       respond_with(@posts)
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @post.update(post_params)
       @posts = @post.restaurant.posts.page params[:page]
       respond_with(@posts)
     else
-      @err = "<dl><dt>#{t('.failurestart')}</dt>" 
+      @err = "<dl><dt>#{t('.failurestart')}</dt>"
       @post.errors.full_messages.map { |msg| @err << "<dd>#{msg}</dd>" }
       @err << "</dl>"
       respond_with(@post)
     end
   end
-  
+
   def destroy
     if @post.destroy
       @posts = @post.restaurant.posts.page params[:page]
       respond_with(@posts)
     end
   end
-  
+
   def show_more
     @all = params[:more] || nil
     @post_id = params[:post_id]
     @restaurant = Restaurant.find(params[:id])
     respond_with(@all)
   end
-  
+
   private
-  
+
   def set_post
     @post = Post.find(params[:id])
   end
-  
+
   def post_params
     params.require(:post).permit(:comment)
   end
